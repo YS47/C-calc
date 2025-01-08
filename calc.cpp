0ff73bb15731a055da55f3f0c17dfda2e4542d54
YS47 <94487960+YS47@users.noreply.github.com>
2025-01-08 23:22:47 +0300

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cmath>
 #include <cctype>                               // converts capitals to lower cases or the other way around to insure case-sensitive inputs.
 #include <string>                               // for string and getline function --> (case-sensitive input cases)
-
+const double Pi = 3.14159;
 using namespace std;
 
-bool mainMenu();                                // Function that display the main menu and take the user choice.
+bool mainMenu();   // Function that display the main menu and take the user choice.
+bool other();
 
 double Sum(double num1, double num2) {          // Function receives two double numbers to find the sum and return it.
 
@@ -66,27 +68,27 @@ void Min(double num1, double num2) {        // function takes two values, check 
         cout << "Both Values are the same! " << endl << endl;
     }
 }
-string D2B(double Decimal)
+
+string D2B(int Decimal)
 {
-    string binary = "" , intbinary,fracbinary=".";
-    double fracdecimal = Decimal - floor(Decimal);
-    int intDecimal = Decimal - fracdecimal;
-    if(intDecimal == 0)
+    string binary = "";
+    if(Decimal == 0)
     {
-        cout << "The binary value of " << Decimal << "is = " << endl << endl;
+        return "0";
     }
     else 
     {
-        while(intDecimal >0)
+        while(Decimal >0)
         {
-            binary = to_string(intDecimal % 2) + binary;
-            intDecimal /= 2;
+            binary = to_string(Decimal % 2) + binary;
+            Decimal /= 2;
         }
         
     }
 
     return binary;
 }
+
 int B2D(int Binary) //converting binary to decimal
 {
     int i = 0, decimal = 0;
@@ -97,14 +99,46 @@ int B2D(int Binary) //converting binary to decimal
     else {
         while (Binary != 0)
         {
-            int digit = Binary % 10;
-            decimal += digit * pow(2, i);
+            int digit = Binary % 10; //to check if its either a 1 or a 0 
+            decimal += digit * pow(2, i); // i is the position of the bit
 
-            Binary /= 10;
+            Binary /= 10; 
             i++;
         }
     }
     return decimal;
+}
+
+double power(double num1, double num2)
+{
+    double result = pow(num1, num2);
+    return result;
+}
+
+double absolute(double num1)
+{
+    double result = abs(num1);
+    return result;
+}
+
+double square_root(double num1)
+{
+    double result = sqrt(num1);
+    return result;
+}
+
+double cosine(double num1)
+{
+    double degree = (num1) * (Pi / 180.0);
+    double result = cos(degree);
+    return result;
+}
+
+double sine(double num1)
+{
+    double degree = (num1) * (Pi / 180.0);
+    double result = sin(degree);
+    return result;
 }
 
 int main()
@@ -238,6 +272,8 @@ bool mainMenu() {           // Display the Menu.
         }
         break;
     }
+
+
     case 7: 
     {
         cout << "+========================== Decimal To Binary Converter  =========================+" << endl<<endl;
@@ -247,6 +283,8 @@ bool mainMenu() {           // Display the Menu.
         cout << "the Value of " << D << " in Binary is : " << D2B(D) << endl << endl;
         break;
     }
+
+
     case 8:
     {
         cout << "+========================== Binary To Decimal Converter  =========================+" << endl << endl;
@@ -254,9 +292,20 @@ bool mainMenu() {           // Display the Menu.
         cout << "Enter the Binary number to convert it into Decimal : ";
         cin >> B;
         B2D(B);
-        cout << "the Binary number "<<B<<"Is equal to " << B2D(B) << "in Decimal " << endl << endl;
+        cout << "the Binary number "<<B<<" Is equal to " << B2D(B) << "in Decimal " << endl << endl;
         break;
     }
+
+
+    case 9: 
+    {
+        do {
+
+        } while (other());
+        break;
+    }
+
+
     case 0: {
 
         cout << "Do You want to quit? " << endl << "1. Yes" << endl << "2. No" << endl << "Your Choice: ";     // make sure the user wants to exit the program.
@@ -284,4 +333,120 @@ bool mainMenu() {           // Display the Menu.
     }
     }
     return true;
+}
+
+bool other()
+{
+    int choice;
+    double num1, num2;
+    cout << "+================================================================ - O X" << endl;
+    cout << "|                                                                     |" << endl;
+    cout << "|                      Choose One Of The Options:                     |" << endl;
+    cout << "|                                                                     |" << endl;
+    cout << "| 1. Power.                                                           |" << endl;
+    cout << "| 2. square root.                                                     |" << endl;
+    cout << "| 3. absolute value.                                                  |" << endl;
+    cout << "| 4. Celling / Floor.                                                 |" << endl;
+    cout << "| 5. cos.                                                             |" << endl;
+    cout << "| 6. sin.                                                             |" << endl;
+    cout << "| 0. Exit.                                                            |" << endl;
+    cout << "|                                                                     |" << endl;
+    cout << "+=====================================================================+" << endl << endl;
+    cout << "Your Choice is : ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        cout << "================================ Power ===============================" << endl << endl;
+        cout << "Enter your number you want to find the power of : ";
+        cin >> num1;
+        cout << "Enter the power : ";
+        cin >> num2;
+        cout << num1 << " To the power of " << num2 <<" Is Equal to = " << power(num1, num2) << endl << endl;
+        break;
+    }
+    case 2:
+    {
+        cout << "============================= square root =============================" << endl << endl;
+        cout << "Enter your number you want to find the square root of : "; 
+        cin >> num1;
+        if (num1 >= 0) {
+            cout << "The square root of " << num1 << " is equal to = " << square_root(num1) << endl << endl;
+        }
+        else 
+        {
+            cout << "Invalid Input... Redirecting Back To Main Menu ......." << endl <<endl;
+            return true;
+        }
+        break;
+    }
+    case 3: 
+    {
+        cout << "=========================== absolute value ===========================" << endl << endl;
+        cout << "Enter your number you want to find the absolute value of :";
+        cin >> num1;
+        cout << "|" << num1 <<"|" << " = " << absolute(num1) << endl << endl;
+        break;
+    }
+    case 4:
+    {
+        cout << "+======================== (Celling / Floor) =======================+" << endl << endl;
+        cout << "What Value Do You Want To Find? " << endl << "1. Celling" << endl << "2. Floor" << endl << "Your choice: ";
+
+        string cf;
+        cin >> cf;
+        for (char& c : cf) {                    // &c to modify the original character in the user input.
+            c = tolower(c);
+        }
+        if (cf == "1" || cf == "celling" || cf == "ceil") {
+            cout << "+======================== (Celling) =======================+" << endl;
+            cout << "Enter a Value To find its celling: ";
+            cin >> num1;
+            cout << "The Celling of " << num1 << " is : " << ceil(num1) << endl << endl;
+        }
+        else if (cf == "2" || cf == "floor" || cf == "floor") {
+            cout << "+======================== (Floor) =======================+" << endl;
+            cout << "Enter a Value To find its floor: ";
+            cin >> num1;
+            cout << "The floor of " << num1 << " is : " << floor(num1) << endl<<endl;
+        }
+        else {
+            cout << "Invalid Input... Redirecting Back To Main Menu ......." << endl;
+        }
+        break;
+    }
+    case 5: 
+    {
+
+    }
+    case 6: 
+    {
+
+    }
+    case 0: 
+    {
+        cout << "Do You want to return to main menu ? " << endl << "1. Yes" << endl << "2. No" << endl << "Your Choice: ";     // make sure the user wants to exit the program.
+
+        string quit;
+        cin >> quit;                                 // string in case the user type "yes", "no" instead of choosing the numbers.
+        for (char& c : quit) {                    // &c to modify the original character in the user input.
+            c = tolower((c));
+        }
+
+        if (quit == "1" || quit == "yes" || quit == "y") {
+            cout << "Redirecting Back To Main Menu ......." << endl;
+            return false;
+        }
+        else if (quit == "2" || quit == "no" || quit == "n") {
+            cout << "Redirecting Back To Other Menu ......." << endl << endl;
+            return true;
+        }
+        else {
+            cout << "Invalid Input... Redirecting Back To Other Menu ......." << endl << endl;
+        }
+        break;
+    }
+    }
+
 }
